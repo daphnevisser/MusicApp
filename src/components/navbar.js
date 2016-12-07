844b93b43a58f149aa90c00d8711d8a8e7f6dcc3
Daphne Visser <hello@daphnevisser.com>
2016-12-07 19:06:31 +0100

    Add links and state to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import { Link, IndexLink } from 'react-router';
 import FontAwesome from 'react-fontawesome';
 
-
 class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {activeTab: 1};
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  handleTabClick(tab) {
+    this.setState({activeTab: tab});
+  }
+
   render() {
     return (
       <nav>
         <ul>
-          <li><FontAwesome name="music" size="2x" className="nav-icon" />Tracks</li>
-          <li><FontAwesome name="star" size="2x" className="nav-icon" />Albums</li>
-          <li><FontAwesome name="id-badge" size="2x" className="nav-icon" />Artists</li>
-          <li><FontAwesome name="search" size="2x" className="nav-icon" />Search</li>
+          <li>
+            <IndexLink to="/" className={(this.state.activeTab === 1) ? "selected nav-link" : "nav-link"} onClick={() => this.handleTabClick(1)}>
+            <FontAwesome name="music" size="2x" className="nav-icon" />Tracks
+            </IndexLink>
+          </li>
+          <li>
+            <Link to="/artists" className={(this.state.activeTab === 2) ? "selected nav-link" : "nav-link"} onClick={() => this.handleTabClick(2)}>
+            <FontAwesome name="star" size="2x" className="nav-icon" />Artists
+            </Link>
+          </li>
+          <li>
+            <Link to="/albums" className={(this.state.activeTab === 3) ? "selected nav-link" : "nav-link"} onClick={() => this.handleTabClick(3)}>
+            <FontAwesome name="id-badge" size="2x" className="nav-icon" />Album
+            </Link>
+          </li>
+          <li>
+            <Link to="/search" className={(this.state.activeTab === 4) ? "selected nav-link" : "nav-link"} onClick={() => this.handleTabClick(4)}>
+            <FontAwesome name="search" size="2x" className="nav-icon" />Search
+            </Link>
+          </li>
         </ul>
       </nav>
     );
