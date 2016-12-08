002bbb62097751c8beaa13a5c593b2e6d1625905
Daphne Visser <hello@daphnevisser.com>
2016-12-08 18:51:58 +0100

    track list styling

--- src/components/tracks-list.js
+++ src/components/tracks-list.js
@@ -2,31 +2,28 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 const TracksList = (props) => {
-  const createListOfTracks = () => {
-    const tracks = props.tracks.results;
-    console.log(tracks);
-    return tracks.map((track) => {
-      return(
-        <li key={track.id}>
-          <a href="#">
-            <FontAwesome name="play-circle-o" size="lg" className="play-icon" />
-            <span className="track-name">{track.name}</span>
-            <span className="track-artist">{track.artist_name}</span>
-          </a>
-        </li>
-      );
-    });
-  };
   if (!props.tracks) {
     return (
       <div className="loading">Loading...</div>
     );
   }
+
+  const listOfTracks = props.tracks.results.map((track) => {
+    return(
+      <li key={track.id}>
+        <a href="#">
+          <FontAwesome name="play-circle-o" size="lg" className="play-icon" />
+          <span className="track-name">{track.name} - </span>
+          <span className="track-artist">{track.artist_name}</span>
+        </a>
+      </li>
+    );
+  });
+
   return (
     <div>
-      Tracklist
-      <ul>
-        {createListOfTracks()}
+      <ul className="list-of-tracks">
+        {listOfTracks}
       </ul>
     </div>
   );
